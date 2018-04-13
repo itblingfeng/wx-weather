7f2ed50b8efa9a465cbff6611da68fab11758274
apple <3394339422@qq.com>
2018-04-13 13:45:01 +0800

    新增阴天界面，天气api接口

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,6 +7,14 @@ Page({
   data: {
     count:0,
     flag: true,
+    latitude:null,
+    location:null,
+    longitude:null,
+    week:[
+      'Mon','Tue','Wed','Thu','Fri','Sat','Sun'
+    ],
+    week_index:null,
+    daily_forecast:[],
   },
 
   /**
@@ -14,42 +22,73 @@ Page({
    */
   onLoad: function (options) {
     var _this = this;
-    const ctx = wx.createCanvasContext('myCanvas1')
-    this.grad(ctx, 'rgb(241, 110, 118)', 'rgb(245,141,121)')
-    ctx.setFontSize(15)
-    ctx.setFillStyle('white')
-    ctx.fillText('Nanjing', 80, 120)
-    ctx.fillText('Monday，Sunday', 40, 150)
-    ctx.setFontSize(70)
-    ctx.fillText('32°',70,240)
-    ctx.draw()
+    wx.getLocation({
+      success: function(data) {
+        _this.setData({
+          longitude: data.longitude,
+          latitude: data.latitude
+        })
+        //请求天气
+        wx.request({
+          url: 'https://free-api.heweather.com/s6/weather/forecast?location=' + data.longitude + ',' + data.latitude +'&key=xxx',
+          success:function(res){
+            var forecast = res.data.HeWeather6[0].daily_forecast
+            var week_index = []
+            for(var i=0;i<forecast.length;i++){
+              var date = new Date(forecast[i].date)
+              week_index.push(date.getDay())
+            }
+            _this.setData({
+              week_index: week_index,
+              location: res.data.HeWeather6[0].basic.location,
+              daily_forecast: forecast
+            })
+            wx.request({
+              url: 'https://free-api.heweather.com/s6/weather/now?location=' + data.longitude + ',' + data.latitude + '&key=xxx',
+              success:function(res){
+                var tmp = res.data.HeWeather6[0].now.tmp+"℃"
+                const ctx = wx.createCanvasContext('myCanvas1')
+                var cond_code = res.data.HeWeather6[0].now.cond_code
+                if (cond_code == 100 || cond_code == 101 || cond_code == 102 || cond_code == 103){
+                  _this.grad(ctx, 'rgb(241,110,118)','rgb(245,141,121)')
+                }else{
+                  _this.grad(ctx, 'rgb(78,104,133)', 'rgb(226,220,214)')
+                  wx.setNavigationBarColor({
+                    frontColor: '#ffffff',
+                    backgroundColor: '#4E6885',
+                  })
+                }
+                ctx.setFontSize(15)
+                ctx.setFillStyle('white')
+                ctx.fillText(res.data.HeWeather6[0].basic.location, 80, 120)
+                var date = new Date();
+                var weekNum = date.getDay();
+                var week = null;
+                switch (weekNum) {
+                  case 0: week = '周日'; break;
+                  case 1: week = '周一'; break;
+                  case 2: week = '周二'; break;
+                  case 3: week = '周三'; break;
+                  case 4: week = '周四'; break;
+                  case 5: week = '周五'; break;
+                  case 6: week = '周六'; break;
+                }
+                ctx.fillText(week + '，' + res.data.HeWeather6[0].now.cond_txt, 80, 150)
+                ctx.setFontSize(70)
+                ctx.fillText(tmp, 70, 240)
+                ctx.draw()
+              }
+            })
+           
+          
+          }
+        })
+
+      },
+    })
+
+
     
-    setInterval(function () {
-      if(_this.data.flag){
-        if (_this.data.count==3){
-          _this.setData({
-            count: _this.data.count - 1,
-            flag: false
-          })
-        }else{
-          _this.setData({
-            count: _this.data.count + 1,
-          })
-        }
-     
-      }else{
-        if (_this.data.count==1){
-          _this.setData({
-            count: _this.data.count - 1,
-            flag: true
-          })
-        }else{
-          _this.setData({
-            count: _this.data.count - 1,
-          })
-        }
-      }
-    }, 800)
     
   },
   
